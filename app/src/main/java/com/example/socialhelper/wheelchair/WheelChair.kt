0be7e4a3ce5716adc782e9e8fb22ca9834662f09
VaasKout <VasyaKotov1@yandex.ru>
2020-09-19 21:12:27 +0300

    Working station choice

--- app/src/main/java/com/example/socialhelper/wheelchair/WheelChair.kt
+++ app/src/main/java/com/example/socialhelper/wheelchair/WheelChair.kt
@@ -1,12 +1,19 @@
 package com.example.socialhelper.wheelchair
 
 import android.app.TimePickerDialog
+import android.content.Context
+import android.content.res.Resources
 import android.os.Bundle
-import android.text.TextWatcher
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.ArrayAdapter
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
 import androidx.core.widget.addTextChangedListener
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
@@ -18,7 +25,6 @@ import com.example.socialhelper.database.WheelData
 import com.example.socialhelper.databinding.FragmentWheelChairBinding
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.snackbar.Snackbar
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import java.text.SimpleDateFormat
 import java.util.*
@@ -27,7 +33,8 @@ class WheelChair : Fragment() {
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?): View? {
+        savedInstanceState: Bundle?
+    ): View? {
 
         val binding: FragmentWheelChairBinding = DataBindingUtil
             .inflate(inflater, R.layout.fragment_wheel_chair, container, false)
@@ -80,6 +87,53 @@ class WheelChair : Fragment() {
             }
         }
 
+        /**
+         * Arrays of stations and their images
+         */
+        val stations =
+            arrayOf(
+                resources.getStringArray(R.array.line_one),
+                resources.getStringArray(R.array.line_two),
+                resources.getStringArray(R.array.line_three),
+                resources.getStringArray(R.array.line_four),
+                resources.getStringArray(R.array.line_five),
+                resources.getStringArray(R.array.line_six),
+                resources.getStringArray(R.array.line_seven),
+                resources.getStringArray(R.array.line_eight),
+                resources.getStringArray(R.array.line_eight_a),
+                resources.getStringArray(R.array.line_nine),
+                resources.getStringArray(R.array.line_ten),
+                resources.getStringArray(R.array.line_eleven),
+                resources.getStringArray(R.array.line_twelve),
+                resources.getStringArray(R.array.line_thirteen),
+                resources.getStringArray(R.array.line_fourteen),
+                resources.getStringArray(R.array.line_fifteen),
+            )
+        val img = arrayOf(
+            R.drawable.circle_one,
+            R.drawable.circle_two,
+            R.drawable.circle_three,
+            R.drawable.circle_four,
+            R.drawable.circle_five,
+            R.drawable.circle_six,
+            R.drawable.circle_seven,
+            R.drawable.circle_eight,
+            R.drawable.circle_eight,
+            R.drawable.circle_nine,
+            R.drawable.circle_ten,
+            R.drawable.circle_eleven,
+            R.drawable.circle_twelve,
+            R.drawable.circle_thirteen,
+            R.drawable.circle_fourteen,
+            R.drawable.circle_fifteen,
+        )
+        val allStations = mutableListOf("")
+        for (line in stations){
+            for (st in line){
+                allStations.add(st)
+            }
+        }
+
         val application = requireNotNull(activity).application
         val timeArray = resources.getStringArray(R.array.time)
         ArrayAdapter.createFromResource(
@@ -98,13 +152,15 @@ class WheelChair : Fragment() {
                     val timeSetListener = TimePickerDialog.OnTimeSetListener { _, hour, minute ->
                         cal.set(Calendar.HOUR_OF_DAY, hour)
                         cal.set(Calendar.MINUTE, minute)
-                         binding.textTimeResult.text =
-                             SimpleDateFormat("HH:mm", Locale.ENGLISH).format(cal.time)
+                        binding.textTimeResult.text =
+                            SimpleDateFormat("HH:mm", Locale.ENGLISH).format(cal.time)
                         viewModel.timeField = binding.textTimeResult.text.toString()
                     }
-                    TimePickerDialog(context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
-                        cal.get(Calendar.MINUTE), true).show()
-                    }
+                    TimePickerDialog(
+                        context, timeSetListener, cal.get(Calendar.HOUR_OF_DAY),
+                        cal.get(Calendar.MINUTE), true
+                    ).show()
+                }
                 else -> {
                     binding.textTimeResult.text = ""
                     viewModel.timeField = binding.textTimeResult.text.toString()
@@ -112,89 +168,197 @@ class WheelChair : Fragment() {
             }
         }
 
-        binding.editStartStation.addTextChangedListener {
-            binding.startStation.error = null
-        }
-        binding.endStationEdit.addTextChangedListener {
-            binding.endStation.error = null
-        }
         binding.timeDropdown.addTextChangedListener {
             binding.timeMenu.error = null
         }
 
         viewModel.send.observe(viewLifecycleOwner, {
-            if (it == true){
-                val startStation = binding.editStartStation.text.toString()
+            if (it == true) {
+                val startStation = binding.startStationEdit.text.toString()
                 val endStation = binding.endStationEdit.text.toString()
                 val time = binding.timeDropdown.text.toString()
                 val comment = binding.commentEdit.text.toString()
 
                 if (startStation.isEmpty()) {
-                        binding.startStation.error = getString(R.string.empty_field_error)
-                        viewModel.onDoneSending()
+                    binding.startStation.error = getString(R.string.empty_field_error)
+                    viewModel.onDoneSending()
                 }
 
                 if (endStation.isEmpty()) {
-                        binding.endStation.error = getString(R.string.empty_field_error)
-                        viewModel.onDoneSending()
+                    binding.endStation.error = getString(R.string.empty_field_error)
+                    viewModel.onDoneSending()
                 }
 
                 if (time.isEmpty()) {
-                        binding.timeMenu.error = getString(R.string.empty_field_error)
-                        viewModel.onDoneSending()
+                    binding.timeMenu.error = getString(R.string.empty_field_error)
+                    viewModel.onDoneSending()
+                }
+                if ( !allStations.contains(startStation) &&
+                    startStation.isNotEmpty()){
+                    binding.startStation.error =
+                        "Выберите станцию, предложенную в выпадающем списке"
+                    viewModel.onDoneSending()
+                }
+
+                if (!allStations.contains(endStation) &&
+                    endStation.isNotEmpty()){
+                    binding.endStation.error =
+                        "Выберите станцию, предложенную в выпадающем списке"
+                    viewModel.onDoneSending()
                 }
 
                 if (startStation.isNotEmpty() &&
-                        endStation.isNotEmpty() &&
-                        time.isNotEmpty()){
-                        viewModel.allInfo.observe(viewLifecycleOwner, {info ->
-                                    val data = WheelData(
-                                        id = 1,
-                                        name = info.name,
-                                        first = startStation,
-                                        second = endStation,
-                                        time = time,
-                                        comment = comment
-                                    )
-                                viewModel.onInsert(data)
+                    endStation.isNotEmpty() &&
+                    time.isNotEmpty() &&
+                    allStations.contains(startStation) &&
+                    allStations.contains(endStation)) {
 
-                                MaterialAlertDialogBuilder(requireContext())
-                                        .setMessage("Данные введены верно?")
-                                        .setNegativeButton("Нет") { _, _ ->
+                    viewModel.allInfo.observe(viewLifecycleOwner, { info ->
+                        val data = WheelData(
+                            id = 1,
+                            name = info.name,
+                            first = startStation,
+                            second = endStation,
+                            time = time,
+                            comment = comment
+                        )
+                        viewModel.onInsert(data)
+
+                        MaterialAlertDialogBuilder(requireContext())
+                            .setMessage("Данные введены верно?")
+                            .setNegativeButton("Нет") { _, _ ->
+                                viewModel.onDoneSending()
+                            }
+                            .setPositiveButton("Да") { _, _ ->
+                                viewModel.data.observe(viewLifecycleOwner, {
+                                    lifecycleScope.launch {
+                                        binding.buttonHelpRequest.isEnabled = false
+                                        binding.buttonHelpRequest.text =
+                                            getString(R.string.wait)
+                                        viewModel.connectToServer()
+                                        viewModel.requestServer()
+                                        if (!viewModel.readWrite.socket.isConnected) {
+                                            Snackbar.make(
+                                                binding.buttonHelpRequest,
+                                                getString(R.string.retry_later),
+                                                Snackbar.LENGTH_SHORT
+                                            ).show()
+                                            binding.buttonHelpRequest.isEnabled = true
+                                            binding.buttonHelpRequest.text =
+                                                getString(R.string.send)
+                                            viewModel.onDoneSending()
+                                        } else {
+                                            this@WheelChair
+                                                .findNavController()
+                                                .navigate(
+                                                    WheelChairDirections
+                                                        .actionWheelChairToWheelChairWait()
+                                                )
                                             viewModel.onDoneSending()
                                         }
-                                        .setPositiveButton("Да") { _, _ ->
-                                            viewModel.data.observe(viewLifecycleOwner, {
-                                        lifecycleScope.launch {
-                                            binding.buttonHelpRequest.isEnabled = false
-                                            binding.buttonHelpRequest.text = getString(R.string.wait)
-                                            viewModel.connectToServer()
-                                            viewModel.requestServer()
-                                            if (!viewModel.readWrite.socket.isConnected) {
-                                                Snackbar.make(
-                                                    binding.buttonHelpRequest,
-                                                    getString(R.string.retry_later),
-                                                    Snackbar.LENGTH_SHORT
-                                                ).show()
-                                                binding.buttonHelpRequest.isEnabled = true
-                                                binding.buttonHelpRequest.text =
-                                                    getString(R.string.send)
-                                                viewModel.onDoneSending()
-                                            } else {
-                                                this@WheelChair
-                                                    .findNavController()
-                                                    .navigate(WheelChairDirections
-                                                        .actionWheelChairToWheelChairWait())
-                                                viewModel.onDoneSending()
-                                            }
-                                        }
-                                            })
-                                        }.show()
-
-                        })
+                                    }
+                                })
+                            }.show()
+                    })
                 }
             }
         })
+
+
+        fun setImg(text: String, imageView: ImageView, textView: TextView){
+
+            imageView.setImageResource(R.drawable.circle)
+            textView.text = ""
+            for(line in stations.indices){
+                if (stations[line].contains(text)){
+                    imageView.setImageResource(img[line])
+                    when {
+                        line < 7 -> {
+                            textView.text = (line+1).toString()
+                            textView.setTextColor(
+                                ContextCompat
+                                    .getColor(requireContext(), R.color.text_white)
+                            )
+
+                        }
+                        line in 7..8 -> {
+                            textView.setTextColor(
+                                ContextCompat
+                                    .getColor(requireContext(), R.color.text_black)
+                            )
+                            if (line == 7){
+                                textView.text = (line+1).toString()
+                            }
+                            else textView.text = getString(R.string.eight_a)
+
+                        } line in 9..stations.size -> {
+                            textView.text = line.toString()
+                        if (line == 14){
+                            textView.setTextColor(
+                                ContextCompat
+                                    .getColor(requireContext(), R.color.text_black)
+                            )
+                        } else{
+                            textView.setTextColor(
+                                ContextCompat
+                                    .getColor(requireContext(), R.color.text_white)
+                            )
+                                }
+                         }
+                    }
+                }
+            }
+        }
+
+
+        val adapter =  ArrayAdapter(
+            requireContext(),
+            R.layout.drop_down_menu, allStations
+        )
+
+        binding.startStationEdit.setAdapter(adapter)
+        binding.endStationEdit.setAdapter(adapter)
+
+        val inputManager: InputMethodManager =
+            requireContext().getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+
+        /**
+         * set *hideSoftInputFromWindow()* only in itemClickListener to prevent bug,
+         * because *fun setImg()* hides keyboard, when you try to type again after
+         * the name of the station is selected
+         */
+
+
+        binding.startStationEdit.addTextChangedListener {
+            binding.startStation.error = null
+            setImg(
+                binding.startStationEdit.text.toString(),
+                binding.circleFirstStation,
+                binding.textFirstCircle
+            )
+        }
+        binding.endStationEdit.addTextChangedListener {
+            binding.endStation.error = null
+            setImg(
+                binding.endStationEdit.text.toString(),
+                binding.circleSecondStation,
+                binding.textSecondCircle
+            )
+        }
+
+        binding.startStationEdit.setOnItemClickListener { _, _, _, _ ->
+            inputManager.hideSoftInputFromWindow(
+                requireActivity().currentFocus?.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+
+        binding.endStationEdit.setOnItemClickListener { _, _, _, _ ->
+            inputManager.hideSoftInputFromWindow(
+                requireActivity().currentFocus?.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS)
+        }
+
+
         binding.lifecycleOwner = this
         return binding.root
     }
